bb1a2f553d71ecc3416eafe9b343bc64c2edb421
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-10-29 19:04:57 -0700

    Fix unit test for creating movies

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -25,14 +25,18 @@ RSpec.describe MoviesController, type: :controller do
 
       # movie = Movie.find_by_title("Iron Man")
       # other_movies = movie.others_by_same_director()
-      expect(Movie.where(title: "Iron Man 2")).to eq 1
+      expect(Movie.where(title: "Iron Man 2").length).to eq 1
       # expect(other_movies).to include(Movie.find_by_title("Spider-Man: Homecoming"))
     end
-
-    it "does not return movies by other directors" do
-      movie = Movie.find_by_title("Big Hero 6")
-      other_movies = movie.others_by_same_director()
-      expect(other_movies).to be_empty
-    end
   end
+
+  # describe "delete method" do
+  #   it "deletes a movie" do
+  #     movie = Movie.where(title: "Big Hero 6")
+  #     # movie = Movie.find_by_title("Iron Man")
+  #     # other_movies = movie.others_by_same_director()
+  #     expect(Movie.where(title: "Big Hero 6").length).to eq 0
+  #     # expect(other_movies).to include(Movie.find_by_title("Spider-Man: Homecoming"))
+  #   end
+  # end
 end
